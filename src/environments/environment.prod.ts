109c7c6484020540a4a43befc4f2b087fb835ebb
Marcelo Alberto Villalpando <37634203+ChazzIV@users.noreply.github.com>
2020-05-27 14:43:42 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://api.themoviedb.org/3',
-  apiKey: 'bdf43cfc640f046d1fdb1c497ef218bb',
+  apiKey: 'YOUR API KEY',
   imgPath: 'https://image.tmdb.org/t/p'
 };
